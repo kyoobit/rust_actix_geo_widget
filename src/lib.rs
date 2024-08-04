65c70b55bfb322dcc0b823e57c22b537804f5c52
Nate Royer <natroyer@Old-mans-M1.local>
2024-08-04 07:15:31 -0600

    add offline option, split lookup functions into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,234 @@
+use std::net::IpAddr;
+
+// A reader for the MaxMind DB format
+// https://docs.rs/maxminddb/latest/maxminddb/
+// http://oschwald.github.io/maxminddb-rust/maxminddb/struct.Reader.html
+// cargo add maxminddb
+use maxminddb::{geoip2, MaxMindDBError, Reader};
+
+// https://docs.rs/serde/latest/serde/
+// https://serde.rs
+use serde::{Deserialize, Serialize};
+
+// LookupAsnResult structure
+#[derive(Clone, Debug, Deserialize)]
+pub struct LookupAsnResult {
+    pub asn: u32,
+    pub asn_organization: String,
+}
+
+/// Return a LookupAsnResult structure for an IP address
+pub fn lookup_asn(
+    asn_database_file: &String,
+    addr: IpAddr,
+    debug: bool,
+    verbose: bool,
+) -> LookupAsnResult {
+    // Default values to be used on any error
+    let asn_result_default = LookupAsnResult {
+        asn: 0,
+        asn_organization: String::from("-"),
+    };
+
+    // Create a handle to the GeoLite2-ASN.mmdb
+    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/index.html
+    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/struct.Asn.html
+    let geo_lite2_asn_reader = Reader::open_readfile(asn_database_file).unwrap();
+
+    // Lookup the ASN information for the IP address
+    let asn_lookup_result: Result<geoip2::Asn, MaxMindDBError> = geo_lite2_asn_reader.lookup(addr);
+
+    // Handle lookup errors gracefully
+    // Unwrap a result or use the default value
+    // Return the result
+    match asn_lookup_result {
+        Ok(result) => LookupAsnResult {
+            asn: result.autonomous_system_number.unwrap(),
+            asn_organization: String::from(result.autonomous_system_organization.unwrap()),
+        },
+        Err(error) => {
+            if debug {
+                println!("lookup_asn(addr: {addr:#?}) error: {error:#?}");
+            }
+            if verbose {
+                //TODO:
+            }
+            asn_result_default
+        }
+    }
+}
+
+/// LookupCityResult structure
+#[derive(Clone, Debug, Deserialize)]
+pub struct LookupCityResult {
+    pub city: String,
+    pub continent: (String, String),
+    pub country: (String, String),
+    pub subdivisions: (String, String),
+}
+
+/// Return a LookupCityResult structure for an IP address
+pub fn lookup_city(
+    city_database_file: &String,
+    addr: IpAddr,
+    debug: bool,
+    verbose: bool,
+) -> LookupCityResult {
+    // Default values to be used on any error
+    let city_result_default = LookupCityResult {
+        city: String::from("-"),
+        continent: (String::from("-"), String::from("-")),
+        country: (String::from("-"), String::from("-")),
+        subdivisions: (String::from("-"), String::from("-")),
+    };
+
+    // Create a handle to the GeoLite2-City.mmdb
+    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/index.html
+    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/struct.Asn.html
+    let geo_lite2_city_reader = Reader::open_readfile(city_database_file).unwrap();
+
+    // Lookup the City information for the IP address
+    let city_lookup_result: Result<geoip2::City, MaxMindDBError> =
+        geo_lite2_city_reader.lookup(addr);
+
+    // Handle lookup errors gracefully
+    // Unwrap a result or use the default value
+    let city_result = match city_lookup_result {
+        Ok(result) => {
+            // <Result>.city -> String
+            let city = match result.city {
+                None => String::from("-"),
+                // TODO: needs a cleaner method like: `result.city?.names.get("en", "-")`
+                _ => result
+                    .city
+                    .unwrap()
+                    .names
+                    .unwrap()
+                    .get("en")
+                    .unwrap()
+                    .to_string(),
+            };
+
+            // <Result>.continent -> (String, String)
+            let continent = match result.continent {
+                None => (String::from("-"), String::from("-")),
+                _ => {
+                    let continent = result.continent.unwrap();
+                    (
+                        continent.code.unwrap().to_string(),
+                        continent.names.unwrap().get("en").unwrap().to_string(),
+                    )
+                }
+            };
+
+            // <Result>.country -> (String, String)
+            let country = match result.country {
+                None => (String::from("-"), String::from("-")),
+                _ => {
+                    let country = result.country.unwrap();
+                    (
+                        country.iso_code.unwrap().to_string(),
+                        country.names.unwrap().get("en").unwrap().to_string(),
+                    )
+                }
+            };
+
+            // <Result>.subdivisions -> (String, String)
+            let subdivisions = match result.subdivisions {
+                None => (String::from("-"), String::from("-")),
+                _ => {
+                    let subdivisions = result.subdivisions.unwrap();
+                    (
+                        subdivisions[0].iso_code.unwrap().to_string(),
+                        subdivisions[0]
+                            .names
+                            .as_ref()
+                            .unwrap()
+                            .get("en")
+                            .unwrap()
+                            .to_string(),
+                    )
+                }
+            };
+
+            // These fields exist in the data but are not used here
+            // <Result>.location
+            // <Result>.postal
+            // <Result>.registered_country
+            // <Result>.traits
+
+            LookupCityResult {
+                city,
+                continent,
+                country,
+                subdivisions,
+            }
+        }
+        Err(error) => {
+            if debug {
+                println!("lookup_city(addr: {addr:#?}) error: {error:#?}");
+            }
+            if verbose {
+                //TODO:
+            }
+            city_result_default
+        }
+    };
+
+    // Return the result
+    city_result
+}
+
+/// LookupResult structure
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct LookupResult {
+    pub address: IpAddr,
+    pub asn: u32,
+    pub asn_organization: String,
+    pub city: String,
+    pub continent: (String, String),
+    pub country: (String, String),
+    pub subdivisions: (String, String),
+    pub summary: String,
+}
+
+/// Return a Lookup summary structure
+pub fn get_summary(asn: &LookupAsnResult, city: &LookupCityResult) -> String {
+    // "<CITY>,<STATE>/<COUNTRY>; <AS NAME> (<ASN>);"
+    let mut summary = String::new();
+    summary.push_str(&city.city);
+    summary.push(',');
+    summary.push_str(&city.subdivisions.0);
+    summary.push('/');
+    summary.push_str(&city.country.0);
+    summary.push_str("; ");
+    summary.push_str(&asn.asn_organization);
+    summary.push_str(" (");
+    summary.push_str(&asn.asn.to_string());
+    summary.push_str(");");
+    summary
+}
+
+/// Return a LookupResult structure for an IP address
+pub fn lookup(
+    asn_database_file: &String,
+    city_database_file: &String,
+    addr: IpAddr,
+    debug: bool,
+    verbose: bool,
+) -> LookupResult {
+    let asn = lookup_asn(asn_database_file, addr, debug, verbose);
+    let city = lookup_city(city_database_file, addr, debug, verbose);
+    let summary = get_summary(&asn, &city);
+
+    LookupResult {
+        address: addr,
+        asn: asn.asn,
+        asn_organization: asn.asn_organization,
+        city: city.city,
+        continent: city.continent,
+        country: city.country,
+        subdivisions: city.subdivisions,
+        summary,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -15,214 +15,12 @@ use clap::Parser;
 // https://docs.rs/env_logger/latest/env_logger/
 // cargo add env_logger
 
-// A reader for the MaxMind DB format
-// https://docs.rs/maxminddb/latest/maxminddb/
-// http://oschwald.github.io/maxminddb-rust/maxminddb/struct.Reader.html
-// cargo add maxminddb
-use maxminddb::{geoip2, MaxMindDBError};
-
 // https://docs.rs/serde/latest/serde/
 // https://serde.rs
 use serde::{Deserialize, Serialize};
 
-/// LookupAsnResult structure
-#[derive(Clone, Debug, Deserialize)]
-struct LookupAsnResult {
-    asn: u32,
-    asn_organization: String,
-}
-
-/// Return a LookupAsnResult structure for an IP address
-fn lookup_asn(
-    asn_database_file: &String,
-    addr: IpAddr,
-    debug: bool,
-    verbose: bool,
-) -> LookupAsnResult {
-    // Default values to be used on any error
-    let asn_result_default = LookupAsnResult {
-        asn: 0,
-        asn_organization: String::from("-"),
-    };
-
-    // Create a handle to the GeoLite2-ASN.mmdb
-    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/index.html
-    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/struct.Asn.html
-    let geo_lite2_asn_reader = maxminddb::Reader::open_readfile(asn_database_file).unwrap();
-
-    // Lookup the ASN information for the IP address
-    let asn_lookup_result: Result<geoip2::Asn, MaxMindDBError> = geo_lite2_asn_reader.lookup(addr);
-
-    // Handle lookup errors gracefully
-    // Unwrap a result or use the default value
-    // Return the result
-    match asn_lookup_result {
-        Ok(result) => LookupAsnResult {
-            asn: result.autonomous_system_number.unwrap(),
-            asn_organization: String::from(result.autonomous_system_organization.unwrap()),
-        },
-        Err(error) => {
-            if debug {
-                println!("lookup_asn(addr: {addr:#?}) error: {error:#?}");
-            }
-            if verbose {
-                //TODO:
-            }
-            asn_result_default
-        }
-    }
-}
-
-/// LookupCityResult structure
-#[derive(Clone, Debug, Deserialize)]
-struct LookupCityResult {
-    city: String,
-    continent: (String, String),
-    country: (String, String),
-    subdivisions: (String, String),
-}
-
-/// Return a LookupCityResult structure for an IP address
-fn lookup_city(
-    city_database_file: &String,
-    addr: IpAddr,
-    debug: bool,
-    verbose: bool,
-) -> LookupCityResult {
-    // Default values to be used on any error
-    let city_result_default = LookupCityResult {
-        city: String::from("-"),
-        continent: (String::from("-"), String::from("-")),
-        country: (String::from("-"), String::from("-")),
-        subdivisions: (String::from("-"), String::from("-")),
-    };
-
-    // Create a handle to the GeoLite2-City.mmdb
-    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/index.html
-    // http://oschwald.github.io/maxminddb-rust/maxminddb/geoip2/struct.Asn.html
-    let geo_lite2_city_reader = maxminddb::Reader::open_readfile(city_database_file).unwrap();
-
-    // Lookup the City information for the IP address
-    let city_lookup_result: Result<geoip2::City, MaxMindDBError> =
-        geo_lite2_city_reader.lookup(addr);
-
-    // Handle lookup errors gracefully
-    // Unwrap a result or use the default value
-    let city_result = match city_lookup_result {
-        Ok(result) => {
-            // <Result>.city -> String
-            let city = match result.city {
-                None => String::from("-"),
-                // TODO: needs a cleaner method like: `result.city?.names.get("en", "-")`
-                _ => result
-                    .city
-                    .unwrap()
-                    .names
-                    .unwrap()
-                    .get("en")
-                    .unwrap()
-                    .to_string(),
-            };
-
-            // <Result>.continent -> (String, String)
-            let continent = match result.continent {
-                None => (String::from("-"), String::from("-")),
-                _ => {
-                    let continent = result.continent.unwrap();
-                    (
-                        continent.code.unwrap().to_string(),
-                        continent.names.unwrap().get("en").unwrap().to_string(),
-                    )
-                }
-            };
-
-            // <Result>.country -> (String, String)
-            let country = match result.country {
-                None => (String::from("-"), String::from("-")),
-                _ => {
-                    let country = result.country.unwrap();
-                    (
-                        country.iso_code.unwrap().to_string(),
-                        country.names.unwrap().get("en").unwrap().to_string(),
-                    )
-                }
-            };
-
-            // <Result>.subdivisions -> (String, String)
-            let subdivisions = match result.subdivisions {
-                None => (String::from("-"), String::from("-")),
-                _ => {
-                    let subdivisions = result.subdivisions.unwrap();
-                    (
-                        subdivisions[0].iso_code.unwrap().to_string(),
-                        subdivisions[0]
-                            .names
-                            .as_ref()
-                            .unwrap()
-                            .get("en")
-                            .unwrap()
-                            .to_string(),
-                    )
-                }
-            };
-
-            // These fields exist in the data but are not used here
-            // <Result>.location
-            // <Result>.postal
-            // <Result>.registered_country
-            // <Result>.traits
-
-            LookupCityResult {
-                city,
-                continent,
-                country,
-                subdivisions,
-            }
-        }
-        Err(error) => {
-            if debug {
-                println!("lookup_city(addr: {addr:#?}) error: {error:#?}");
-            }
-            if verbose {
-                //TODO:
-            }
-            city_result_default
-        }
-    };
-
-    // Return the result
-    city_result
-}
-
-/// Return a Lookup summary structure
-fn get_summary(asn: &LookupAsnResult, city: &LookupCityResult) -> String {
-    // "<CITY>,<STATE>/<COUNTRY>; <AS NAME> (<ASN>);"
-    let mut summary = String::new();
-    summary.push_str(&city.city);
-    summary.push(',');
-    summary.push_str(&city.subdivisions.0);
-    summary.push('/');
-    summary.push_str(&city.country.0);
-    summary.push_str("; ");
-    summary.push_str(&asn.asn_organization);
-    summary.push_str(" (");
-    summary.push_str(&asn.asn.to_string());
-    summary.push_str(");");
-    summary
-}
-
-/// LookupResult structure
-#[derive(Serialize)]
-struct LookupResult {
-    address: IpAddr,
-    asn: u32,
-    asn_organization: String,
-    city: String,
-    continent: (String, String),
-    country: (String, String),
-    subdivisions: (String, String),
-    summary: String,
-}
+// IP information lookup
+use actix_geo_widget::lookup;
 
 /// RequestPath structure
 #[derive(Debug, Deserialize)]
@@ -237,28 +35,16 @@ async fn address(data: web::Data<AppData>, path: web::Path<RequestPath>) -> Resu
     // TODO: Conversion error handling -> 400 Client Error
     let address = path.address.parse::<IpAddr>().unwrap();
 
-    // Lookup the ASN information for the IP address
+    // Lookup the information for the IP address
     let asn_database_file = &data.asn_database_file;
-    let asn_result = lookup_asn(asn_database_file, address, data.debug, data.verbose);
-
-    // Lookup the City information for the IP address
     let city_database_file = &data.city_database_file;
-    let city_result = lookup_city(city_database_file, address, data.debug, data.verbose);
-
-    // Get a summary of the information
-    let summary = get_summary(&asn_result, &city_result);
-
-    // ...
-    let result = LookupResult {
+    let result = lookup(
+        asn_database_file,  // --asn-database-file
+        city_database_file, // --city-database-file
         address,
-        asn: asn_result.asn,
-        asn_organization: asn_result.asn_organization,
-        city: city_result.city,
-        continent: city_result.continent,
-        country: city_result.country,
-        subdivisions: city_result.subdivisions,
-        summary,
-    };
+        data.debug,   // --debug
+        data.verbose, // --verbose
+    );
 
     // Format the result into JSON
     // https://docs.rs/actix-web/latest/actix_web/web/struct.Json.html
@@ -388,7 +174,7 @@ async fn actix_main(args: Args) -> std::io::Result<()> {
     version = None,
 )]
 struct Args {
-    /// The IP address to listen for requests
+    /// The IP address to listen for requests (IP address to lookup in offline mode)
     #[arg(short, long, default_value = "0.0.0.0")]
     addr: String,
 
@@ -404,6 +190,10 @@ struct Args {
     #[arg(long, default_value = "GeoLite2-City.mmdb")]
     city_database_file: String,
 
+    /// Offline mode (IP address to lookup taken from -a/--addr)
+    #[arg(short, long)]
+    offline: bool,
+
     /// Increase log messaging to verbose
     #[arg(short, long)]
     verbose: bool,
@@ -416,5 +206,19 @@ struct Args {
 // CLI configuration options using clap
 fn main() {
     let args = Args::parse();
-    let _ = actix_main(args);
+
+    // Lookup the IP address information
+    if args.offline {
+        let result = lookup(
+            &args.asn_database_file,
+            &args.city_database_file,
+            args.addr.parse::<IpAddr>().unwrap(),
+            args.debug,
+            args.verbose,
+        );
+        println!("{:?}", result);
+    // Start the web service
+    } else {
+        let _ = actix_main(args);
+    }
 }
